178265c5550629d758217e3addcb39c308e5c2ef
Nicola Pasquali <nicola_pasquali@outlook.it>
2018-04-17 21:59:13 +0200

    Initial commit

---
+++ src/test/java/com/npasquali/dbfileloader/MainTest.java
@@ -0,0 +1,15 @@
+package com.npasquali.dbfileloader;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Nicola Pasquali
+ */
+public class MainTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
