e46d41b71bfa68a8e0a79fc4cc9c0482180da260
Nicola Pasquali <nicola_pasquali@outlook.it>
2018-04-19 22:50:34 +0200

    Now data can be batch inserted into the database

--- src/main/java/com/npasquali/dbfileloader/fileloaders/CSVFileLoader.java
+++ src/main/java/com/npasquali/dbfileloader/fileloaders/CSVFileLoader.java
@@ -4,6 +4,9 @@ import com.google.inject.internal.util.Lists;
 import com.npasquali.dbfileloader.models.CSVFile.CSVFile;
 import com.npasquali.dbfileloader.parsers.CSVParser;
 import org.apache.log4j.Logger;
+import org.sql2o.Connection;
+import org.sql2o.Query;
+import org.sql2o.Sql2o;
 
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -14,12 +17,15 @@ import java.util.List;
 public class CSVFileLoader extends AbstractFileLoader {
     private CSVParser csvParser;
     private List<CSVFile> files;
+    private Sql2o sql2o;
 
     public CSVFileLoader(String basePath, List<String> filesName) {
         logger = Logger.getLogger(CSVFileLoader.class);
 
         this.basePath = basePath;
         initializeFiles(filesName);
+        this.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/test", "test", "test");
+        loadDataIntoDatabase();
     }
 
     private void initializeFiles(List<String> filesName) {
@@ -35,6 +41,28 @@ public class CSVFileLoader extends AbstractFileLoader {
         } catch (FileNotFoundException e) {
             // TODO Find a better way to handle this
             System.out.println("Not found");
+        }
+    }
+
+    public void loadDataIntoDatabase() {
+        final String sql = "INSERT INTO people(firstname, lastname, age) VALUES (:firstname, :lastname, :age)";
+
+        try (Connection con = sql2o.beginTransaction()) {
+            final Query query = con.createQuery(sql);
+
+            files.forEach(file -> {
+                file.getRecords().forEach(record -> {
+                            query
+                                    .addParameter("firstname", record.get(0))
+                                    .addParameter("lastname", record.get(1))
+                                    .addParameter("age", Integer.valueOf(record.get(2)))
+                                    .addToBatch();
+                        }
+                );
+            });
+
+            query.executeBatch(); // executes entire batch
+            con.commit();         // remember to call commit(), else sql2o will automatically rollback.
         }
     }
 }
